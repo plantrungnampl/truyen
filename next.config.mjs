04f6dbca08ec6415983bc9712add4742d88107d1
antrungnam <antrungnam1234@gmail.com>
2024-10-17 23:02:22 +0700

    fix prisma

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals.push('@prisma/client')
+        }
+        return config
+    },
     images: {
         remotePatterns: [
             {
